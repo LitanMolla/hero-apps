b9a21ec3dbca3ea7c4c80ef88fe48ee3d1a82cab
LitanMolla <litanmolla9@gmail.com>
2025-10-09 23:49:42 +0600

    home page spinner update

--- src/components/TrendingApps/TrendingApps.jsx
+++ src/components/TrendingApps/TrendingApps.jsx
@@ -2,13 +2,14 @@ import Container from '../Container/Container'
 import AppCard from '../AppCard/AppCard'
 import { Link } from 'react-router'
 import useAppData from '../../hooks/useAppData'
+import Spinner from '../Spinner/Spinner'
 
 const TrendingApps = () => {
-    const { apps } = useAppData();
+    const { apps, pending } = useAppData();
     const sliceData = apps?.slice(0, 8);
     return (
         <>
-            <div className="my-10 md:my-20">
+            {pending ? <Spinner /> : <div className="my-10 md:my-20">
                 <Container>
                     <h2 className='text-3xl md:text-5xl text-center font-bold'>Trending Apps</h2>
                     <p className='text-gray-900/60 text-center mt-4'>Explore All Trending Apps on the Market developed by us</p>
@@ -19,7 +20,7 @@ const TrendingApps = () => {
                         <Link to='/apps' className='bg-gradient-to-r from-purple-600 via-purple-500 to-purple-400 px-7 py-3 rounded-sm text-gray-100 duration-300 hover:bg-gradient-to-l'>Show All</Link>
                     </div>
                 </Container>
-            </div>
+            </div>}
         </>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,15 @@ import Hero from '../../components/Hero/Hero'
 import Achievement from '../../components/Achievement/Achievement'
 import TrendingApps from '../../components/TrendingApps/TrendingApps'
 import useAppData from '../../hooks/useAppData'
-import Spinner from '../../components/Spinner/Spinner'
 
-const Home = () => {99
+const Home = () => {
+  99
   const { pending } = useAppData();
   return (
     <>
-      {
-        pending
-          ?
-          <Spinner />
-          : <>
-            <Hero />
-            <Achievement />
-            <TrendingApps />
-          </>}
+      <Hero />
+      <Achievement />
+      <TrendingApps />
     </>
   )
 }
